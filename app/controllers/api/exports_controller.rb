02fb9035f2334cf8b5ee8099db76f5121e6de670
Enver Dzhaparoff <ed@netlab.pro>
2017-12-09 17:03:48 +0300

    init commit

--- app/controllers/api/exports_controller.rb
+++ app/controllers/api/exports_controller.rb
@@ -16,7 +16,7 @@ class Api::ExportsController < Api::ApiController
   def export
     @exports = Export.find(params[:ids])
 
-    client = Faraday.new('https://vse-generatori.ru')
+    client = Faraday.new('https://vse-generatori.ru', { ssl: { verify: false } })
     config = Hash.new
     config[:deactivate_not_founded] = Array.new
 
